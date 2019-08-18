792a7f7ee81192f044bcb2b3c0f7548bec279277
ashuomble5 <ashuomble5@gmail.com>
2019-08-18 10:54:44 +0530

    Updates

---
+++ precache-manifest.6a706336ceced1ab66d6f1567a18f4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf560c11966cd4109780b92f3cf4ebe",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "b85cb78b308be6e15f24",
+    "url": "/react-redux-todo/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c9687b882bc81ac60a73",
+    "url": "/react-redux-todo/static/css/main.1041a4b2.chunk.css"
+  },
+  {
+    "revision": "b85cb78b308be6e15f24",
+    "url": "/react-redux-todo/static/js/2.556717c9.chunk.js"
+  },
+  {
+    "revision": "c9687b882bc81ac60a73",
+    "url": "/react-redux-todo/static/js/main.f0485f14.chunk.js"
+  },
+  {
+    "revision": "589f08fa2e443d1bb660",
+    "url": "/react-redux-todo/static/js/runtime~main.5daec96c.js"
+  }
+]);
